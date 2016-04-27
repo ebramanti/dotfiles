fb1941a8f87fae7d668dc8c1b120b0620b1e2977
Edward Bramanti <edward@bramanti.org>
2016-04-26 23:09:47 -0700

    Clean up comments in nvm install

--- node/nvm/install.sh
+++ node/nvm/install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
-# Source bash
 export NVM_DIR="/Users/Edward/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 nvm install node
