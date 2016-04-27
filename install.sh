f6a1cc42d687b4ec40a9013a5330590784409835
Edward Bramanti <edward@bramanti.org>
2016-04-26 21:17:32 -0700

    Update the main install script

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@ echo "Installing your Dotfiles!"
 echo ""
 
 # Brew
+echo "---> Preinstall"
+xcode-select --install
+echo "---> Preinstall done"
+
+# Brew
 echo ""
 echo "---> Brew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
 cd ~/.dotfiles/brew/ && brew bundle
 cd ~/.dotfiles/
@@ -21,12 +27,28 @@ ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 echo "---> Git done"
 
 echo ""
-echo "---> NPM"
-python ~/.dotfiles/npm/install.py
-echo "---> NPM done"
+echo "---> Ruby (rbenv)"
+bash ~/.dotfiles/ruby/rbenv/install.sh
+echo "---> Ruby done"
 
 echo ""
-echo "---> fish"
+echo "---> Python (pyenv)"
+bash ~/.dotfiles/ruby/rbenv/install.sh
+echo "---> Ruby done"
+
+echo ""
+echo "---> Go (gvm)"
+bash ~/.dotfiles/go/gvm.sh
+echo "---> Ruby done"
+
+echo ""
+echo "---> Node"
+bash ~/.dotfiles/node/nvm/install.sh
+python ~/.dotfiles/node/npm/install.py
+echo "---> Node done"
+
+echo ""
+echo "---> Fish"
 mkdir -p ~/.config/
-ln -s ~/.dotfiles/fisherman ~/.config/fisherman
-echo "---> fish done"
+ln -s ~/.dotfiles/fish ~/.config/fish
+echo "---> Fish done"
