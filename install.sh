9728ac882b05a5360accdb1c997503f261381e7b
Edward Bramanti <edward@bramanti.org>
2026-03-25 11:35:53 -0600

    Homebrew fixes - Discord Canary naming - Remove old cask

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ echo ""
 echo "---> Brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-brew tap homebrew/cask-versions
 brew bundle --file=$DOTFILES/brew/Brewfile
 touch $DOTFILES/brew/local/Brewfile
 echo "---> Brew done"
